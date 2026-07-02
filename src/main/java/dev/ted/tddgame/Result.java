19800761ed113132e9619e463dfe7adefb7ac887
Ted M. Young <tedyoung@gmail.com>
2026-07-02 13:01:44 -0700

    Add Result for use by Command execution return

---
+++ src/main/java/dev/ted/tddgame/Result.java
@@ -0,0 +1,70 @@
+package dev.ted.tddgame;
+
+public sealed abstract class Result<SUCCESS, FAILURE> {
+
+    public static <SUCCESS, FAILURE> Result<SUCCESS, FAILURE> success(SUCCESS value) {
+        return new SuccessResult<>(value);
+    }
+
+    public static <SUCCESS, FAILURE> Result<SUCCESS, FAILURE> failure(FAILURE message) {
+        return new FailureResult<>(message);
+    }
+
+    public abstract SUCCESS value();
+
+    public abstract boolean isSuccess();
+
+    public boolean isFailure() {
+        return !isSuccess();
+    }
+
+    public abstract FAILURE failureInfo();
+
+
+    private static final class SuccessResult<SUCCESS, FAILURE> extends Result<SUCCESS, FAILURE> {
+        private final SUCCESS value;
+
+        private SuccessResult(SUCCESS value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return true;
+        }
+
+        @Override
+        public SUCCESS value() {
+            return value;
+        }
+
+        @Override
+        public FAILURE failureInfo() {
+            return null;
+        }
+    }
+
+    private static final class FailureResult<SUCCESS, FAILURE> extends Result<SUCCESS, FAILURE> {
+        private final FAILURE failureInfo;
+
+        private FailureResult(FAILURE failureInfo) {
+            this.failureInfo = failureInfo;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return false;
+        }
+
+        @Override
+        public SUCCESS value() {
+            return null;
+        }
+
+        @Override
+        public FAILURE failureInfo() {
+            return failureInfo;
+        }
+    }
+
+}
---
+++ src/test/java/dev/ted/tddgame/ResultTest.java
@@ -0,0 +1,38 @@
+package dev.ted.tddgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ResultTest {
+
+    @Test
+    void successIsSuccessful() {
+        Result<Event, String> successfulEvent = Result.success(
+                new GameCreated(1L, "irrelevant", "irrelevant", "irrelevant"));
+
+        assertThat(successfulEvent.isSuccess())
+                .isTrue();
+        assertThat(successfulEvent.isFailure())
+                .isFalse();
+        assertThat(successfulEvent.value())
+                .isEqualTo(new GameCreated(1L, "irrelevant", "irrelevant", "irrelevant"));
+        assertThat(successfulEvent.failureInfo())
+                .isNull();
+    }
+
+    @Test
+    void failureIsNotSuccess() {
+        Result<Event, String> failure = Result.failure("Oops, failed!");
+
+        assertThat(failure.isSuccess())
+                .isFalse();
+        assertThat(failure.isFailure())
+                .isTrue();
+        assertThat(failure.value())
+                .isNull();
+        assertThat(failure.failureInfo())
+                .isEqualTo("Oops, failed!");
+    }
+
+}
